8c9752e154b6d76fca516d8b69853aeac355a0bb
gichira22 <gichiravirginia@gmail.com>
2025-03-21 23:05:33 +0300

    Initialize Django project and real estate app

---
+++ backend/realestate/views.py
@@ -0,0 +1,27 @@
+# from django.shortcuts import render
+
+# # Create your views here.
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from .serializers import RegisterSerializer, LoginSerializer, UserSerializer
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
+
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.validated_data, status=status.HTTP_200_OK)
