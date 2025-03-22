9c84bfecb9bb45af79e491fb4bf50aa3ea97008b
gichira22 <gichiravirginia@gmail.com>
2025-03-22 16:23:40 +0300

    Modify backend/__pycache__/urls.cpython-313.pyc Create a new file backend/__pycache__/wsgi.cpython-313.pyc Update the backend/db.sqlite3 by creating more users Modify backend/realestate/__pycache__/admin.cpython-313.pyc backend/realestate/__pycache__/serializers.cpython-313.pyc  backend/realestate/__pycache__/urls.cpython-313.pyc  backend/realestate/__pycache__/views.cpython-313.pyc Adjust realestate/admin.py file to change admin header, title, and urls Add login and register paths in realestate/urls.py

--- backend/realestate/admin.py
+++ backend/realestate/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+admin.site.site_header = "Admin"
+admin.site.site_title = "Admin Panel"
+admin.site.site_url = "Welcome to Prime Residence Admin Panel!"
